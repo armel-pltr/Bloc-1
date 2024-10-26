c5822a66018670360ba6c3e4f8355f09747e7c98
unknown <armelpltr14@gmail.com>
2024-10-26 15:06:52 +0200

    message.php

---
+++ docs/pages/td5/message.php
@@ -0,0 +1,15 @@
+
+
+
+<div>
+    <a href="?msg=Message 1">Message 1</a>
+    <a href="?msg=Message 2">Message 2</a>
+    <a href="?msg=Message 3">Message 3</a>
+
+    <?php
+    $msg=$_GET['msg'];
+
+    echo "<h1>$msg</h1>";
+    ?>
+</div>
+
